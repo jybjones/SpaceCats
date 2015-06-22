278fa09bc6ca567216cf6981a6cb51ab838df7b0
Jennifer Jones <jybjones@gmail.com>
2015-06-22 10:21:22 -0500

    added barriers to screen

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv',
 var player;
 var starfield;
 var background;
+var cursors;
+var bank;
+
+var ACCELERATION = 600;
+var DRAG = 400;
+var MAXSPEED = 400;
 
 
 function preload() {
@@ -16,17 +22,51 @@ function create() {
     //  The scrolling starfield background
     starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
 
-    //  The hero!
+     //  The hero!
     player = game.add.sprite(400, 500, 'ship');
     player.anchor.setTo(0.5, 0.5);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.maxVelocity.setTo(MAXSPEED, MAXSPEED);
+    player.body.drag.setTo(DRAG, DRAG);
+
+    //  And some controls to play the game with
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
     //  Scroll the background
     starfield.tilePosition.y += 2;
 
+    //  Reset the player, then check for movement keys
+    player.body.acceleration.x = 0;
+
+     if (cursors.left.isDown)
+    {
+        player.body.acceleration.x = -ACCELERATION;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.acceleration.x = ACCELERATION;
+    }
+
+    // stop at the screen edges//
+    if (player.x > game.width -50) {
+      player.x = game.width -50;
+      player.body.acceleration.x = 0;
+    }
+    if (player.x < 50) {
+      player.x =50;
+      player.body.acceleration.x = 0;
+    }
+    //  Squish and rotate ship for illusion of "banking"
+    bank = player.body.velocity.x / MAXSPEED;
+    player.scale.x = 1 - Math.abs(bank) / 2;
+    player.angle = bank * 10;
 }
+
 
 function render() {
 
 }
+
+game.state.start('Game');
